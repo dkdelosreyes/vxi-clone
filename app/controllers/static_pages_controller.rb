17733cb09167afbfe8b2d15fe7b4d5f135cd6e7f
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2018-09-18 22:42:54 +0800

    talkpush api test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,29 @@
+require 'uri'
+require 'net/http'
+
 class StaticPagesController < ApplicationController
   def home
+
+  	url = URI("https://my.talkpush.com/api/talkpush_services/campaigns/4/campaign_invitations")
+
+		http = Net::HTTP.new(url.host, url.port)
+
+		request = Net::HTTP::Patch.new(url)
+		request["Content-Type"] = 'application/json'
+		request["Cache-Control"] = 'no-cache'
+		request.body = '{
+		    "api_key": "dbed2ab32d1e9f1453d1a321480debca",
+		    "api_secret": "a7281c8ff4ad9d0d78ae640d4c63b64c",
+		    "campaign_invitation": {
+		      "first_name": "John",
+		      "last_name": "Peters",
+		      "email": "dkdelosreyes@gmail.com",
+		      "user_phone_number": "09063897290"
+		    }
+		  }'
+		response = http.request(request)
+		puts response.read_body
+
   end
 
   def blog

beb4b78fd6fcb93251049dc1323986da9e7cc7ea
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2018-09-19 20:55:20 +0800

    devise setup, users controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default URL options for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'vxi.herokuapp.com', port: 3000 }
 end

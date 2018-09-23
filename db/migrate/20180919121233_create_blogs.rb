888c0a3533ace85e4dcbf457425c8275b5bdd065
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2018-09-23 22:47:03 +0800

    test heroku

--- db/migrate/20180919121233_create_blogs.rb
+++ db/migrate/20180919121233_create_blogs.rb
@@ -1,7 +1,7 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
-    	t.string :title, limit: 50, null: false
+    	t.string :title, limit: 255, null: false
     	t.text :description
     	t.integer :user_id
     	t.integer :status_id

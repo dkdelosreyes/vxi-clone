4dfc52f679cee18131c0e14a36eacab16e02859f
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2018-09-24 00:24:49 +0800

    tp api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	# Redirect invalid routes to root in production
-	match '*path' => redirect('/'), via: [:get, :post] unless Rails.env.development?
+	# match '*path' => redirect('/'), via: [:get, :post] unless Rails.env.development?
   
   # Public paths
 	root to: 'pages#home'
@@ -12,17 +12,17 @@ Rails.application.routes.draw do
 	post 'signup_vxi', to: 'applicants#signup_vxi'
 
 	# Admin paths
-	devise_for :users, path: 'admin', skip: :registrations
-	get 'admin', to: 'admin/blogs#index'
-	namespace :admin do
-		resources :posts
-		resources :users
-		resources :blogs
-		resources :statuses
-		resources :locations
-		resources :source_infos
-		resources :experiences
-		resources :educations
-		resources :positions
-	end
+	# devise_for :users, path: 'admin', skip: :registrations
+	# get 'admin', to: 'admin/blogs#index'
+	# namespace :admin do
+	# 	resources :posts
+	# 	resources :users
+	# 	resources :blogs
+	# 	resources :statuses
+	# 	resources :locations
+	# 	resources :source_infos
+	# 	resources :experiences
+	# 	resources :educations
+	# 	resources :positions
+	# end
 end

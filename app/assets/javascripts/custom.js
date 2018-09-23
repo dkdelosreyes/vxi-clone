807b6b181732f9b0b4a2baed91f3b84849c3711e
Dianne Delos Reyes <dkdelosreyes@gmail.com>
2018-09-23 22:17:45 +0800

    test fb

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,55 +8,7 @@ if( document.getElementById("HomePage") ) {
 	console.log('Home');
 	
 
-	/* FB API  */
-	function getUserData() {
-	  FB.api('/me', {fields: 'first_name, last_name'}, function(response) {
-	    $('#applicant_first_name').val(response.first_name);
-	    $('#applicant_last_name').val(response.last_name);
-	    console.log('getUserData', response);
-	  });
-	}
-	/* Commented out to remove localhost error */
-	window.fbAsyncInit = function() {
-	  //SDK loaded, initialize it
-	  FB.init({
-	    appId      : '929935887215359',
-	    xfbml      : true,
-	    version    : 'v2.8'
-	  });
-	 console.log('fbAsyncInit');
-	  //check user session and refresh it
-	  FB.getLoginStatus(function(response) {
-	    if (response.status === 'connected') {
-	      //user is authorized
-	      getUserData();
-	    } else {
-	      //user is not authorized
-	      console.log('please log in');
-	    }
-	  });
-	};
-	 
-	//load the JavaScript SDK
-	(function(d, s, id){
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) {return;}
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.com/en_US/sdk.js";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-	 
-	//add event listener to login button
-	document.getElementById('BtnFB').addEventListener('click', function(e) {
-	  e.preventDefault();
-	  //do the login
-	  FB.login(function(response) {
-	    if (response.authResponse) {
-	      //user just authorized your app
-	      getUserData();
-	    }
-	  }, {scope: 'email,public_profile', return_scopes: true});
-	}, false);
+	
 }
 
 
